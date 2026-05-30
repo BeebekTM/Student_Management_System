6b13da5996fecf14046ee67c8dceb00c22380c23
IGbeebek <beebektamang90@gmail.com>
2026-05-30 21:56:03 +0545

    Initial Commit

---
+++ students/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Student
+
+def student_list(request):
+    students = Student.objects.all()
+
+    print(students)  # DEBUG (important)
+
+    return render(request, 'students/student_list.html', {
+        'students': students
+    })
