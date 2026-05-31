628028e79d227ae9a59e40f3d901bd6850f2989f
IGbeebek <beebektamang90@gmail.com>
2026-05-31 07:05:21 +0545

    CRUD Operations

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('students/', views.student_list, name='student_list'),
+    path('students/add/', views.student_create, name='student_create'),
+    path('students/<int:pk>/edit/', views.student_update, name='student_update'),
+    path('students/<int:pk>/delete/', views.student_delete, name='student_delete'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
+from .form import StudentForm
+from django.shortcuts import get_object_or_404
+
 
 def student_list(request):
     students = Student.objects.all()
@@ -9,3 +12,56 @@ def student_list(request):
     return render(request, 'students/student_list.html', {
         'students': students
     })
+
+def student_create(request):
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+
+    else:
+        form = StudentForm()
+
+    return render(
+        request,
+        'students/student_form.html',
+        {'form': form}
+    )
+
+
+def student_update(request, pk):
+
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+
+        if form.is_valid():
+            form.save()
+            return redirect("student_list")
+
+    else:
+        form = StudentForm(instance=student)
+
+    return render(
+        request,
+        "students/student_form.html",
+        {"form": form}
+    )
+
+def student_delete(request, pk):
+
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == "POST":
+        student.delete()
+        return redirect("student_list")
+
+    return render(
+        request,
+        "students/student_confirm_delete.html",
+        {"student": student}
+    )
